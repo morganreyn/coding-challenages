a150740055a2d98a229b3e00ca500cb13e464e05
Morgan Reynolds <morganreynolds@Morgans-MacBook-Pro.local>
2020-09-15 15:14:50 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#Coding challenge solutions 
+While doing coding challenges, I like to keep the solution and the original prompt so that I can refer back to them later.
+
+* https://codingbat.com/java
---
+++ src/Strings.java
@@ -0,0 +1,44 @@
+public class Strings {
+
+    public static void main (String[] args) {
+        System.out.println(catDog("ccatdogcatat"));
+    }
+
+    /**
+     * Given a string, return a string where for every char in the original, there are two chars.
+     *
+     * doubleChar("The") → "TThhee"
+     * doubleChar("AAbb") → "AAAAbbbb"
+     * doubleChar("Hi-There") → "HHii--TThheerree"
+     */
+    public static String doubleChar(String str) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            result.append(str.charAt(i)).append(str.charAt(i));
+        }
+        return result.toString();
+    }
+
+    /**
+     * Return true if the string "cat" and "dog" appear the same number of times in the given string.
+     *
+     * catDog("catdog") → true
+     * catDog("catcat") → false
+     * catDog("1cat1cadodog") → true
+     */
+    public static boolean catDog(String str) {
+        int cats = countSubString(str, "cat");
+        int dogs = countSubString(str, "dog");
+        return cats == dogs;
+    }
+    public static int countSubString(final String input, final String sub) {
+        int count = 0;
+        int idx = 0;
+        while (input.indexOf(sub, idx) != -1) {
+            count++;
+            idx = input.indexOf(sub, idx) + 1;
+        }
+        return count;
+    }
+
+}
